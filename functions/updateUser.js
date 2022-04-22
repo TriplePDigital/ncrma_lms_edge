16d486e145f241ad8c82bdc56b213c0013aaacad
Daniel Papp <dpapp001@odu.edu>
2022-04-22 17:51:52 -0400

    worked on update user functionality

--- functions/updateUser.js
+++ functions/updateUser.js
@@ -1,6 +1,22 @@
-exports = async function(request, response) {
-    let collection = await context.services.get("mongodb-atlas").db("myFirstDatabase").collection("users");
-    let res = await collection.updateOne({email: request.query.email}, { $set: { image: request.query.image, name: request.query.fullName } })
-    
-    return res
+exports = async function (request, response) {
+  try {
+    let collection = await context.services
+      .get("mongodb-atlas")
+      .db("myFirstDatabase")
+      .collection("users");
+    let res = await collection.updateOne(
+      { _id: request.query.id },
+      {
+        $set: {
+          email: `${request.query.email}`,
+          name: `${request.query.firstName} ${request.query.lastName}`,
+        },
+      }
+    );
+
+    return res;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
 };
